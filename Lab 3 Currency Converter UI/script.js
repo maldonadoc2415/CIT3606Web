86433c01ece2804040e7734bef397baf332ef3da
maldonadoc2415 <maldonadoc2415@student.elms.edu>
2025-09-17 22:03:10 -0400

    Lab 3 completed, Extra credit done

--- Lab 3 Currency Converter UI/script.js
+++ Lab 3 Currency Converter UI/script.js
@@ -1,3 +1,23 @@
-const ='USD';
-const ='Rupiah';
+// Define UI elements
+
+const USD = document.getElementById("USD");
+const RUP = document.getElementById("RUP");
+
+
+// Define conversion functions
+function convertToRup() {
+    let usd = parseFloat(USD.value);
+    let rup = usd * 16467
+    RUP.value = rup.toFixed(2);
+    
+
+
+    
+}
+function convertToUsd() {
+    let rup = parseFloat(RUP.value);
+    let usd = rup / 16467
+    USD.value = usd.toFixed(2);
+}
+    
 
