fc7ff75dc061482c43385fd0008f4d6bfe0eeab6
maldonadoc2415 <maldonadoc2415@student.elms.edu>
2025-10-26 23:28:50 -0400

    Lab 6 done and lab 7

---
+++ Lab 7 React Pokedex/react-demo/src/App.js
@@ -0,0 +1,41 @@
+import logo from './logo.svg';
+import './App.css';
+
+function MyButton(props) {
+  return (
+    <button className="button" onClick={handler}>{props.text}</button>
+  );
+}
+function handler() {
+  alert("You clicked me!");
+}
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Chris
+          <MyButton className="button" text="Chris' Button"></MyButton>
+        </h1>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        
+      </header>
+
+    </div>
+
+  );
+}
+
+
+
+export default App;
