73ef11aca5f2fbce23fb882c421d3fbc799d893c
maldonadoc2415 <maldonadoc2415@student.elms.edu>
2025-10-29 21:26:17 -0400

    Lab 7 part 1 complete

--- Lab 7 React Pokedex/pokedex/src/App.js
+++ Lab 7 React Pokedex/pokedex/src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+const pokemon = [
+    {
+        mon: "Pikachu",
+    img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png",
+    type: "Electric",
+    id: 25,
+    summary: "Pikachu that can generate powerful electricity have cheek sacs that are extra soft and super stretchy."
+    },
+
+    {
+    mon: "Charmander",
+    img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png",
+    type: "Fire",
+    id: 4,
+    summary: "The flame that burns at the tip of its tail is an indication of its emotions. The flame wavers when Charmander is enjoying itself."
+    },
+    {
+    mon: "Squirtle",
+    img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/7.png",
+    type: "Water",
+    id: 7,
+    summary: "Squirtle's shell is not merely used for protection. The shell's rounded shape and the grooves on its surface help minimize resistance in water, enabling this Pokémon to swim at high speeds."
+    },
+    {
+    mon: "Bulbasaur",
+    img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png",
+    type: "Grass/Poison",
+    id: 1,
+    summary: "There is a plant seed on its back right from the day this Pokémon is born. The seed slowly grows larger."
+    }
+];
+
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Welcome to the Pokedex App!
+        <div className="Pokedex">
+            {pokemon.map((poke) => (
+                <div key={poke.id} className="Pokemon-card">
+                    <h2>{poke.mon}</h2>
+                    <img src={poke.img} alt={poke.mon} />
+                    <li><strong>Type:</strong> {poke.type}</li>
+                    <li><strong>ID:</strong> {poke.id}</li>
+                    
+                </div>
+            ))}
+        </div>
       </header>
+        
+        
+      
     </div>
   );
 }
 
 export default App;
+
+
