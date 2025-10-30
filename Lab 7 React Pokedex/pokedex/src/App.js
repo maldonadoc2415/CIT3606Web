1dd9eb4e7f0b32a6a1a1c5cf142d84d83c57d3d8
maldonadoc2415 <maldonadoc2415@student.elms.edu>
2025-10-29 22:32:53 -0400

    final touches to lab 7 part 1

--- Lab 7 React Pokedex/pokedex/src/App.js
+++ Lab 7 React Pokedex/pokedex/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
-import useState from 'react';
+
 
 
 const pokemon = [
     {
         mon: "Pikachu",
-    img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png",
+    img: "images/pikachu.png",
     type: "Electric",
     id: 25,
     summary: "Pikachu that can generate powerful electricity have cheek sacs that are extra soft and super stretchy."
@@ -14,21 +14,21 @@ const pokemon = [
 
     {
     mon: "Charmander",
-    img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png",
+    img: "images/charmander.png",
     type: "Fire",
     id: 4,
     summary: "The flame that burns at the tip of its tail is an indication of its emotions. The flame wavers when Charmander is enjoying itself."
     },
     {
     mon: "Squirtle",
-    img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/7.png",
+    img: "images/squirtle.png",
     type: "Water",
     id: 7,
     summary: "Squirtle's shell is not merely used for protection. The shell's rounded shape and the grooves on its surface help minimize resistance in water, enabling this Pokémon to swim at high speeds."
     },
     {
     mon: "Bulbasaur",
-    img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png",
+    img: "images/bulbasaur.png",
     type: "Grass/Poison",
     id: 1,
     summary: "There is a plant seed on its back right from the day this Pokémon is born. The seed slowly grows larger."
@@ -46,9 +46,9 @@ function App() {
             {pokemon.map((poke) => (
                 <div key={poke.id} className="Pokemon-card">
                     <h2>{poke.mon}</h2>
-                    <img src={poke.img} alt={poke.mon} />
+                    <img className="pokeImage"src={poke.img} alt={poke.mon} />
                     <li><strong>Type:</strong> {poke.type}</li>
-                    <li><strong>ID:</strong> {poke.id}</li>
+                    <li>ID:{poke.id}</li>
                     <p className="App-p">{poke.summary}</p>
                 </div>
             ))}
