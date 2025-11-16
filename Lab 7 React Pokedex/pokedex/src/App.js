0d49e7d83db3a67a749c78df6e22df87470c83ab
maldonadoc2415 <maldonadoc2415@student.elms.edu>
2025-11-16 16:15:39 -0500

    lab 8 stuff

--- Lab 7 React Pokedex/pokedex/src/App.js
+++ Lab 7 React Pokedex/pokedex/src/App.js
@@ -54,6 +54,8 @@ function PokemonGrabber() {
       .catch(err => {
         console.error(err);
         setImage(""); 
+
+        
       });
   };
 
---
+++ Lab 8 NodeJS Database/index.js
@@ -0,0 +1,24 @@
+
+const express = require('express'); 
+const app = express();
+app.get('/', function(req, res){
+   res.send(
+    "<input type='text' value='Hello World from NodeJS and MySQL Database!' /><button"
+  
+    + " onclick='alert(document.querySelector(\"input\").value)'>Click Me!</button>"
+    );
+});
+app.listen(8080);
+
+const mysql = require('mysql');
+require('dotenv').config();               
+const conn = mysql.createConnection({
+  host: "mysql1-p2.ezhostingserver.com",
+  database: "citdemo",
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD
+});
+conn.connect((err) => {           // can move this into app.get and send
+  if (err) throw err;
+  console.log("Connected!");
+});
